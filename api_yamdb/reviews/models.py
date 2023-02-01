e18092d0831de926439016e91d7f2249dd398619
Сергей Трегубов <tregubovsergei2016@yandex.ru>
2023-02-01 17:12:10 +0300

    Заполнил редми и дописал модели

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Review(models.Model):
+    """Модель отзывов и рейтингов"""
+    title = models.ForeignKey(
+        #Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        #User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    pub_date = models.DateTimeField(
+        'Дата отзыва',
+        auto_now_add=True,
+        db_index=True
+    )
+    text = models.TextField()
+    score = models.IntegerField(
+        'Оценка',
+        default=0,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ],
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'], name="unique_review")
+        ]
+
+
+class Comment(models.Model):
+    """Модель комментариев"""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        #User,
+        on_delete=models.CASCADE,
+        #related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        'Дата комментария',
+        auto_now_add=True,
+        db_index=True
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return self.author
